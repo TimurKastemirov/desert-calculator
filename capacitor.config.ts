3d7c6b859dd18ca905ea55b29e4a678f3bf8f734
tymur <tamerlan8.05.92@gmail.com>
2022-10-31 11:48:07 +0300

    add capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'dessert-calculator',
+  webDir: 'dist/dessert-calculator',
+  bundledWebRuntime: false
+};
+
+export default config;
