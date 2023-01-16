525aae6f6762adfb10ab334d5bb62d6c8a538218
tymur <tamerlan8.05.92@gmail.com>
2023-01-16 11:38:43 +0300

    add cordova-res for icon and splash screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'dessert-calculator',
+  appName: 'Dessert Calculator',
   webDir: 'dist/dessert-calculator',
   bundledWebRuntime: false
 };
